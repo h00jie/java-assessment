5147fc41e2fd11bc7c3ab811c3426adf5c29e797
h00jie <panos.hd@gmail.com>
2023-07-20 03:20:26 +0300

    init

--- src/main/java/lib/DI.java
+++ src/main/java/lib/DI.java
@@ -12,25 +12,25 @@ import java.util.Map;
 public class DI {
     private final Map<Class<?>, Object> singletonInstances = new HashMap<>();
 
-    public <T> T oneOf(Class<T> clazz) {
-        return createInstance(clazz);
+    public <T> T oneOf(Class<T> cls) {
+        return createInstance(cls);
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T singletonOf(Class<T> clazz) {
-        if (singletonInstances.containsKey(clazz)) {
-            return (T) singletonInstances.get(clazz);
+    public <T> T singletonOf(Class<T> cls) {
+        if (singletonInstances.containsKey(cls)) {
+            return (T) singletonInstances.get(cls);
         }
-        T instance = createInstance(clazz);
-        singletonInstances.put(clazz, instance);
+        T instance = createInstance(cls);
+        singletonInstances.put(cls, instance);
         return instance;
     }
 
-    public <T> List<T> listOf(Class<T> clazz) {
+    public <T> List<T> listOf(Class<T> cls) {
         List<T> instances = new ArrayList<>();
         String packageName = "gr.pricefox.implementions";
         Reflections reflections = new Reflections(packageName, new SubTypesScanner(false));
-        for (Class<? extends T> implementor : reflections.getSubTypesOf(clazz)) {
+        for (Class<? extends T> implementor : reflections.getSubTypesOf(cls)) {
             try {
                 instances.add(implementor.getDeclaredConstructor().newInstance());
             } catch (Exception e) {
@@ -41,9 +41,9 @@ public class DI {
     }
 
     @SuppressWarnings("unchecked")
-    private <T> T createInstance(Class<T> clazz) {
+    private <T> T createInstance(Class<T> cls) {
         try {
-            Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+            Constructor<?>[] constructors = cls.getDeclaredConstructors();
             for (Constructor<?> constructor : constructors) {
                 Class<?>[] parameterTypes = constructor.getParameterTypes();
                 Object[] dependencies = new Object[parameterTypes.length];
@@ -61,7 +61,7 @@ public class DI {
             }
             return null;
         } catch (Exception e) {
-            throw new RuntimeException("Cannot create instance of " + clazz.getSimpleName());
+            throw new RuntimeException("Cannot create instance of " + cls.getSimpleName());
         }
     }
 
